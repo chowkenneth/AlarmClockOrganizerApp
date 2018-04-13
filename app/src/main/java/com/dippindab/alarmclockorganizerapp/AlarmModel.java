eeb61f728046493f4136aca427d9458500aee836
chowkenneth <kchow95@gmail.com>
2018-04-12 22:18:46 -0500

    Added alarm class, receiver, and folder

---
+++ app/src/main/java/com/dippindab/alarmclockorganizerapp/AlarmModel.java
@@ -0,0 +1,12 @@
+package com.dippindab.alarmclockorganizerapp;
+
+import android.provider.AlarmClock;
+
+public class AlarmModel {
+    private int ID;
+    private AlarmClock alarmClock;
+
+    public void setAlarmClock(AlarmClock alarmClock) {
+        this.alarmClock = alarmClock;
+    }
+}
